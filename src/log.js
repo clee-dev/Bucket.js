7dd53a8bc371d0cbfca45b1d4028354d481ed577
Cody Lee <codyleedev@gmail.com>
2019-12-14 21:38:00 -0800

    log() only print message.content, not whole object

--- src/log.js
+++ src/log.js
@@ -6,7 +6,7 @@ module.exports = function log(client, sourceMessage, ...args) {
     const logChannels = client.channels.filter(c => logChannelIDs.includes(c.id));
     logChannels.forEach(channel => channel.send(
         (sourceMessage ?
-            'message: ' + sourceMessage + '\r\n' + 
+            'message: ' + sourceMessage.author.username + ': ' + sourceMessage.content + '\r\n' + 
             'channel: ' + sourceMessage.channel.name + '\r\n' : '') +
         args.map(a => JSON.stringify(a)).join('\r\n')
     ));
